564085b659d71312b63b31c03f2b6e1265c03fdf
Olesya Volkova <olesyavolkova2705@gmail.com>
2025-06-20 23:18:39 +0300

    ошибки

--- src/components/feed-info/feed-info.tsx
+++ src/components/feed-info/feed-info.tsx
@@ -1,52 +1,36 @@
-import React, { FC, memo } from 'react';
+import { FC } from 'react';
 
-import styles from './feed-info.module.css';
+import { TOrder } from '@utils-types';
+import { FeedInfoUI } from '../ui/feed-info';
+import { useSelector } from '../../services/store';
+import {
+  ordersSelector,
+  totalSelector,
+  totalTodaySelector
+} from '../../services/slices/feed/slice';
 
-import { FeedInfoUIProps, HalfColumnProps, TColumnProps } from './type';
+const getOrders = (orders: TOrder[], status: string): number[] =>
+  orders
+    .filter((item) => item.status === status)
+    .map((item) => item.number)
+    .slice(0, 20);
 
-export const FeedInfoUI: FC<FeedInfoUIProps> = memo(
-  ({ feed, readyOrders, pendingOrders }) => {
-    const { total, totalToday } = feed;
+export const FeedInfo: FC = () => {
+  const orders: TOrder[] = useSelector(ordersSelector);
+  const feed = {
+    total: useSelector(totalSelector),
+    totalToday: useSelector(totalTodaySelector)
+  };
 
-    return (
-      <section>
-        <div className={styles.columns}>
-          <HalfColumn
-            orders={readyOrders}
-            title={'Готовы'}
-            textColor={'blue'}
-          />
-          <HalfColumn orders={pendingOrders} title={'В работе'} />
-        </div>
-        <Column title={'Выполнено за все время'} content={total} />
-        <Column title={'Выполнено за сегодня'} content={totalToday} />
-      </section>
-    );
-  }
-);
+  const readyOrders = getOrders(orders, 'done');
 
-const HalfColumn: FC<HalfColumnProps> = ({ orders, title, textColor }) => (
-  <div className={`pr-6 ${styles.column}`}>
-    <h3 className={`text text_type_main-medium ${styles.title}`}>{title}:</h3>
-    <ul className={`pt-6  ${styles.list}`}>
-      {orders.map((item, index) => (
-        <li
-          className={`text text_type_digits-default ${styles.list_item}`}
-          style={{ color: textColor === 'blue' ? '#00cccc' : '#F2F2F3' }}
-          key={index}
-        >
-          {item}
-        </li>
-      ))}
-    </ul>
-  </div>
-);
+  const pendingOrders = getOrders(orders, 'pending');
 
-const Column: FC<TColumnProps> = ({ title, content }) => (
-  <>
-    <h3 className={`pt-15 text text_type_main-medium ${styles.title}`}>
-      {title}:
-    </h3>
-    <p className={`text text_type_digits-large ${styles.content}`}>{content}</p>
-  </>
-);
+  return (
+    <FeedInfoUI
+      readyOrders={readyOrders}
+      pendingOrders={pendingOrders}
+      feed={feed}
+    />
+  );
+};
